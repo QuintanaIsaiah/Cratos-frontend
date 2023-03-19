e458545a11a02b13388e193b4a1427f7c947334d
Isaiah <isaiahquierasmus@gmail.com>
2023-03-19 21:44:41 +0100

    Creo estructura genera. Carga el inicio con login y registro. Pendiente configurar politica CORS / backend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import Inicio from './Inicio';
 import Login from "./Inicio/Login";
 import Register from "./Inicio/Register";
+import Main from "./Main";
+import MainAdmin from "./MainAdmin";
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -14,6 +16,8 @@ root.render(
           <Route path="/" element={<Inicio/>}></Route>
           <Route path="/Login" element={<Login/>}></Route>
           <Route path="/Register" element={<Register/>}></Route>
+          <Route path="/Main" element={<Main/>}></Route>
+          <Route path="/MainAdmin" element={<MainAdmin/>}></Route>
         </Routes>
       </BrowserRouter>
 );
