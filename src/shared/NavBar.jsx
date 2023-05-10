0a506ff046a09bb59263f34911d4075434f85c08
rorosana <r.a.caraballo8@gmail.com>
2023-05-10 18:53:46 +0200

    Maquetación de NavBar

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -2,17 +2,22 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../index.css";
 import {Link} from "react-router-dom";
 import Usuario from "../Inicio/Usuario";
+import logo_cratos from '../Main/img/logo_cratos.svg';
 
 const NavBar = () => {
 
 
   return (
     <div className="NavBarContainer">
-      <Link to={"/"} className="linkLogin">CRATOS</Link>
+      <Link to={"/"} className="logo"><img src={logo_cratos} alt="Logo de la empresa" /></Link>
+    <div className="navLinksContainer">
       <Link to={"/Login"} className="linkLogin">Log In</Link>
+      <span>|</span>
       <Link to={"/Register"} className="linkLogin">Register</Link>
+      <span>|</span>
       <Link to={"/Carro"} className="linkLogin">Carro</Link>
       <Usuario></Usuario>
+    </div>
     </div>
   );
 };
